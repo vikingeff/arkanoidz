88a0220ab303457a532220a05577327e484040bc
vikingeff <mail.leger@gmail.com>
2015-05-02 10:04:36 +0200

    files

---
+++ Makefile
@@ -0,0 +1,55 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: gleger <gleger@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2013/12/26 11:20:41 by gleger            #+#    #+#              #
+#    Updated: 2015/05/02 09:52:27 by gleger           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = arkanoid
+
+HEADERS = -I libft/ -I ./glfw/include/glfw/
+
+LIBFT = libft/libft.a
+
+SOURCES = main.c \
+
+OBJECTS = $(SOURCES:.c=.o)
+
+HFILE = ./glfw/include/glfw/glfw3.h
+
+FLAGS = -g -Wall -Wextra -Werror -O3
+
+GLFW = -Lglfw/src -lglfw3 -framework glut -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
+
+CC	=	clang
+
+all: $(NAME)
+
+gitsub:
+	git submodule update --init
+
+$(NAME): $(OBJECTS) $(HFILE)
+	@echo "\033[1;35;m[Linking] \t\t\033[0m: " | tr -d '\n'
+	$(CC) $(FLAGS) $(GLFW) -o $@ $(OBJECTS)
+	@echo "\033[1;32;m[Success] \t\t\t\033[0m"
+
+%.o:		%.c $(HPP)
+	@echo "\033[1;36;m[Compiling $<]\t\033[0m: " | tr -d '\n'
+	$(CC) -c $< $(HEADERS)
+
+clean:
+	@echo "\033[0;33;m[Cleaning objects]\033[0m"
+	@rm -f $(OBJECTS)
+
+fclean: clean
+	@echo "\033[0;31;m[Deleting $(NAME)]\033[0m"
+	@rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ main.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gleger <gleger@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/02 08:32:52 by gleger            #+#    #+#             */
+/*   Updated: 2015/05/02 09:53:13 by gleger           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void error_callback(int error, const char* description)
+{
+	fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int			main(int argc, char **argv)
+{
+	GLFWwindow* window;
+	glfwSetErrorCallback(error_callback);
+
+	if (argc == 1)
+		printf ("%s\n",argv[1]);
+	
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 16);
+
+	window = glfwCreateWindow(640, 480, "Arkanoid 3000 (C) VikingZ", NULL, NULL);
+	
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	glfwSetKeyCallback(window, key_callback);
+	
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+	glDepthFunc(GL_LESS);
+	// const GLubyte *renderer = glGetString(GL_RENDERER);
+ //    const GLubyte *version = glGetString(GL_VERSION);
+
+ //    write_log("Starting GLFW %s\n", glfwGetVersionString());
+ //    write_log("Renderer: %s\n", renderer);
+ //    write_log("OpenGL version supported %s\n", version);
+	while (!glfwWindowShouldClose(window))
+	{
+	    float ratio;
+	    int width, height;
+	    glfwGetFramebufferSize(window, &width, &height);
+	    ratio = width / (float) height;
+	    glViewport(0, 0, width, height);
+	    glClear(GL_COLOR_BUFFER_BIT);
+	    glMatrixMode(GL_PROJECTION);
+	    glLoadIdentity();
+	    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+	    glMatrixMode(GL_MODELVIEW);
+	    glLoadIdentity();
+	    glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+	    glBegin(GL_TRIANGLES);
+	    glColor3f(1.f, 0.f, 0.f);
+	    glVertex3f(-0.6f, -0.4f, 0.f);
+	    glColor3f(0.f, 1.f, 0.f);
+	    glVertex3f(0.6f, -0.4f, 0.f);
+	    glColor3f(0.f, 0.f, 1.f);
+	    glVertex3f(0.f, 0.6f, 0.f);
+	    glEnd();
+	    glfwSwapBuffers(window);
+	    glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return (0);
+}
